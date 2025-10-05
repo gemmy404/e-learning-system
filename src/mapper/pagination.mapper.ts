59755f541db435a634614514ea65b864aa6bd810
Mahmoud Gamal <mahmoudgemmy581@gmail.com>
2025-10-05 19:53:15 +0300

    add category route and fix lastPage property in pagination mapper

--- src/mapper/pagination.mapper.ts
+++ src/mapper/pagination.mapper.ts
@@ -1,12 +1,13 @@
 import {PageResponse} from "../dto/page.response.ts";
 
 export const toPageResponse = (size: number, page: number, counts: number): PageResponse => {
+   const totalPages = Math.ceil(counts / size);
     return {
         currentPage: page,
         pageSize: size,
         totalElements: counts,
-        totalPages: Math.ceil(counts / size),
+        totalPages,
         firstPage: page === 1,
-        lastPage: page >= Math.ceil(counts / size),
+        lastPage: page === totalPages,
     };
 }
--- src/routes/main.route.ts
+++ src/routes/main.route.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import {router as authRouter} from './auth.route';
 import {router as courseRouter} from './course.route';
+import {router as categoryRouter} from './category.route';
 import {router as adminRouter} from './admin.route';
 import {router as instructorRouter} from './instructor.route';
 import {router as userRouter} from './user.route';
@@ -12,6 +13,7 @@ export const router = express.Router();
 
 router.use('/auth', authRouter);
 router.use('/courses', courseRouter);
+router.use('/categories', categoryRouter)
 router.use('/users', verifyToken, userRouter);
 router.use('/admin', verifyToken, allowTo(UserRole.ADMIN), adminRouter);
 router.use('/instructor', verifyToken, allowTo(UserRole.INSTRUCTOR), instructorRouter);
